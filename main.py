ae948d2d2a1faeccae55e3c4b5f9ee6f70209c73
ryuxinn <vhfaria15@gmail.com>
2023-08-30 01:05:36 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from shutil import unpack_archive,move
 from pathlib import Path
+from utils.check_files import check_type_game
 from time import sleep
 from rarfile import RarFile
 import py7zr
+
 
 class Organizer:
     def __init__(self,inp_dir):
@@ -22,23 +24,17 @@ class Organizer:
                     print(new_folder)
 
     def unzip_game(self):
+        # Checando se possui algum arquivo download.part
         for game in self.download_dir.iterdir():
             while True:
                 if ".part" in game.suffix and game.exists():
-                    print(".part in download")
-                    print(game)
+                    print(f"{game} in download")
                     sleep(10)
                 else:
                     break
-
+        
         for game in self.download_dir.iterdir():
-            if ".part" in game.suffix:
-                while True:
-                    print(".part in download")
-                    print(game)
-                    sleep(10)
-
-            elif ".zip" in game.suffix:
+            if ".zip" in game.suffix:
                 unpack_archive(game,self.inp_dir / "Games")
                 print(f"unpack - {game}")
 
@@ -69,55 +65,7 @@ class Organizer:
         emulators_dir = self.inp_dir / "Emulators"
 
         for game in games_dir.iterdir():
-            match (game.suffix):
-                case ".gb":
-                    file = emulators_dir / "GameBoy" / game.name
-                    print(f"Moving {game} -> {file}") # mover para /gameboy
-                    move(game,file)
-                case ".gba" | ".GBA":
-                    file =  emulators_dir / "GBA" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Atari/Atari 2600
-                    move(game,file)         
-                case ".gbc" | ".GBC":
-                    file =  emulators_dir / "GBColor" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Atari/Atari 2600
-                    move(game,file)
-                case ".nes":
-                    file = emulators_dir /"NES" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /nes
-                    move(game,file)                    
-                case ".smc" | ".sfc":
-                    file = emulators_dir /"SNES" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /snes
-                    move(game,file)
-                case ".z64" | ".n64":
-                    file =  emulators_dir / "N64" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /N64
-                    move(game,file)
-                case ".a26":
-                    file =  emulators_dir / "Atari/Atari 2600" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Atari/Atari 2600
-                    move(game,file)
-                case ".a52":
-                    file =  emulators_dir / "Atari/Atari 5200" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Atari/Atari 5200
-                    move(game,file)
-                case ".a78":
-                    file =  emulators_dir / "Atari/Atari 7800" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Atari/Atari 7800
-                    move(game,file)
-                case ".wbfs":
-                    file =  emulators_dir / "Wii" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Wii
-                    move(game,file)
-                case ".md":
-                    file =  emulators_dir / "Genesis" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Genesis
-                    move(game,file)
-                case ".nds":
-                    file =  emulators_dir / "Nintendo DS" / game.name
-                    print(f"Moving - {game} -> {file}") # mover para /Nintendo DS
-                    move(game,file)
+            check_type_game(emulators_dir,game)
 
 def run():
     o = Organizer("/data/sata/IKONOS")
