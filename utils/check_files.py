f825a19adbe0f6b9eda8c0881e88b7cc6620bfd8
XuRy <82833077+ryuxinn@users.noreply.github.com>
2024-05-23 23:38:42 -0300

    Fixing Unbound Error

--- utils/check_files.py
+++ utils/check_files.py
@@ -8,32 +8,57 @@ def check_type_game(emulators_dir,game):
     match (game.suffix):
         case ".gb":
             file = emulators_dir / "GameBoy" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".gba" | ".GBA":
             file =  emulators_dir / "GBA" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".gbc" | ".GBC":
             file =  emulators_dir / "GBColor" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".nes":
             file = emulators_dir /"NES" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".smc" | ".sfc":
             file = emulators_dir /"SNES" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".z64" | ".n64":
             file =  emulators_dir / "N64" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".a26":
             file =  emulators_dir / "Atari/Atari 2600" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".a52":
             file =  emulators_dir / "Atari/Atari 5200" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".a78":
             file =  emulators_dir / "Atari/Atari 7800" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".wbfs" | ".rvz" | ".gcz":
             file =  emulators_dir / "Wii" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".md":
             file =  emulators_dir / "Genesis" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".nds":
             file =  emulators_dir / "Nintendo DS" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
         case ".3ds":
             file =  emulators_dir / "Nintendo 3DS" / game.name
+            print(f"Moving - {game} -> {file}")
+            move(game,file)            
         case ".wux":
             file =  emulators_dir / "Wii U" / game.name
-            
-    print(f"Moving - {game} -> {file}") # mover para /Wii U
-    move(game,file)
+            print(f"Moving - {game} -> {file}")
+            move(game,file)
